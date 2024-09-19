2a6d5fa429d4d6244347ae50d70d17ce92be33ca
Michal Wiaderek <wiaderekmichal@gmail.com>
2024-09-19 17:42:37 +0200

    feature: add e2e tests

---
+++ e2e/cart.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, Page } from '@playwright/test';
+
+const APP_URL = 'http://localhost:3000/';
+
+const addItemToACart = async (page: Page) => {
+  await page.goto(APP_URL);
+
+  const items = await page.locator('data-test-id=e2eProductContainer');
+
+  await items.nth(1).getByText('Add to cart').click();
+
+  await page.goto(`${APP_URL}/cart`);
+
+  const cartItems = await page.locator('data-test-id=e2eCartItem');
+  
+  const cartItemsCount = await cartItems.count();
+
+  expect(cartItemsCount).toEqual(1);
+}
+
+test('adding an item to the cart', async ({ page }) => {
+  await addItemToACart(page);
+});
+
+test('deleting an item from the cart', async ({ page }) => {
+  await addItemToACart(page);
+
+  const itemToDelete = await page.locator('data-test-id=e2eCartItem').nth(0);
+
+  await itemToDelete.getByAltText('Trash icon').click();
+
+  const cartItems = await page.locator('data-test-id=e2eCartItem');
+  
+  const cartItemsCount = await cartItems.count();
+
+  expect(cartItemsCount).toEqual(0);
+});
